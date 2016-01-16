ac93058f446fe848dcd761571c49560bd6a17620
victordetoni <victordetoni@gmail.com>
2016-01-16 18:55:32 -0200

    Update splaytree1.c

--- splaytree1.c
+++ splaytree1.c
@@ -16,14 +16,7 @@ pthread_t thread_1, thread_2;
 int
 intcmp(struct list_el *e1, struct list_el *e2)
 {
-/*
-    if(e1->idx != -9280)
-        return (e1->idx < e2->idx ? -1 : e1->idx > e2->idx);
-        else if (e1->val1 != 32767)
-            return (e1->val1 < e2->val1 ? -1 : e1->val1 > e2->val1);
-     
-//  return (e1->tv.tv_sec < e2->tv.tv_sec ? -1 : e1->tv.tv_sec > e2->tv.tv_sec);*/
-    return (e1->idx < e2->idx ? -1 : e1->idx > e2->idx);
+   return (e1->idx < e2->idx ? -1 : e1->idx > e2->idx);
 }
 
 SPLAY_HEAD(inttree, list_el) head = SPLAY_INITIALIZER(&head);
@@ -49,34 +42,22 @@ void *gettime() {
 
 void *addNumber() {
 
-    for(;;) {
-    struct list_el find;
-
+   for(;;) {
+         struct list_el find;
         curr = (struct list_el *)malloc(sizeof(struct list_el));
         curr->val1 = i+i;
         curr->tv.tv_sec = tv.tv_sec;
         curr->idx = i;
         SPLAY_INSERT(inttree, &head, curr);
 
-    /*  
- 
-        */
-//        find.val1 = curr->val1;
-
- //       if(SPLAY_FIND(inttree,&head,&find))
-   //         printf("found %d\n", curr->val1);
         i++;
-        //SPLAY_FOREACH(var, inttree, &head) {
         var = SPLAY_MIN(inttree, &head);
             if((tv.tv_sec - var->tv.tv_sec) > 3) {
                 SPLAY_REMOVE(inttree, &head, var);
                 free(var);
             }
         var = SPLAY_MAX(inttree, &head);
-            //break;
-        //  printf("%ld %ld %d\n",var->idx, var->tv.tv_sec, var->val1);
-        //}*/
-}
+   }
 }
 
 int main() {
